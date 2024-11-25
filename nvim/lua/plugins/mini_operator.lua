18e6aa95682feb5aa14f22b030c5469a8a6d39ab
errorlike <errorlikeman@outlook.com>
2024-11-25 23:01:24 +0800

    添加从剪贴板替换快捷键

--- nvim/lua/plugins/mini_operator.lua
+++ nvim/lua/plugins/mini_operator.lua
@@ -5,5 +5,6 @@ return {
     sort = { prefix = "" },
     evaluate = { prefix = "" },
     exchange = { prefix = "" },
+    vim.keymap.set({ "n", "v" }, "<leader>gr", '"*gr', { remap = true, desc = "replace from clipboard" }),
   },
 }
