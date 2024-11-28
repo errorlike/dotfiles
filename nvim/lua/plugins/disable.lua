ae29001603aea7818779d22c923981125b4f6f4f
errorlike <errorlikeman@outlook.com>
2024-11-28 23:53:15 +0800

    删除visual_star 添加tiny-inline-diagnostic

--- nvim/lua/plugins/disable.lua
+++ nvim/lua/plugins/disable.lua
@@ -1,3 +1,13 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    {
+      "nvim-lspconfig",
+      opts = {
+        diagnostics = {
+          virtual_text = false,
+        },
+      },
+    },
+  },
 }
