e10e4e6cf288f179573dec60065e40b519dc8258
errorlike <errorlikeman@outlook.com>
2024-12-19 12:22:23 +0800

    修改undotree的keymap,避免和snack nvim 冲突

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -2,6 +2,6 @@ return {
   "mbbill/undotree",
   config = function()
     vim.g.undotree_SetFocusWhenToggle = 1
-    vim.keymap.set("n", "<leader>ua", vim.cmd.UndotreeToggle, { desc = "undotree" })
+    vim.keymap.set("n", "<F5>", vim.cmd.UndotreeToggle, { desc = "undotree" })
   end,
 }
