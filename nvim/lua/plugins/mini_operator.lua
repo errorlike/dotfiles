d50e19686247261d62d1db8e9732f27027a03228
errorlike <errorlikeman@outlook.com>
2025-01-07 00:30:59 +0800

    修复从剪贴板替换不生效的问题

--- nvim/lua/plugins/mini_operator.lua
+++ nvim/lua/plugins/mini_operator.lua
@@ -1,12 +1,10 @@
 return {
   "echasnovski/mini.operators",
   version = "*",
-  keys = {
-    { mode = { "n", "v" }, "<leader>gr", '"*gr', remap = true, desc = "replace from clipboard" },
-  },
   opts = {
     sort = { prefix = "" },
     evaluate = { prefix = "" },
     exchange = { prefix = "" },
+    vim.keymap.set({ "n", "v" }, "<leader>gr", '"*gr', { remap = true, desc = "replace from clipboard" }),
   },
 }
