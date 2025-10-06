52b2f1f1ab252f53895f991b97c7b5bc07f119fe
errorlike <errorlikeman@gmail.com>
2025-10-06 21:27:30 +0800

    删除undotree插件

--- nvim/lua/plugins/undotree.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "mbbill/undotree",
-  config = function()
-    vim.g.undotree_SetFocusWhenToggle = 1
-    vim.keymap.set("n", "<F5>", vim.cmd.UndotreeToggle, { desc = "undotree" })
-  end,
-}
