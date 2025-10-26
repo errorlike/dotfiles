67cd75fbb176126bbfd1c462ef49aa09490d3c3f
errorlike <errorlikeman@gmail.com>
2025-10-26 10:48:54 +0800

    添加im-select插件

---
+++ nvim/lua/plugins/im-select.lua
@@ -0,0 +1,9 @@
+return {
+  -- "keaising/im-select.nvim",
+  -- config = function()
+  --   require("im_select").setup({
+  --     default_command = "im-select-mspy.exe",
+  --     default_im_select = "英语模式",
+  --   })
+  -- end,
+}
